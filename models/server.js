7a94702009dad414f5f0688f5c227c503c03dc60
Matias Straface <93721908+mgstraface@users.noreply.github.com>
2022-12-01 11:21:24 -0300

    default port updated

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 class Server {
 	constructor() {
 		this.app = express();
-		this.port = process.env.PORT;
+		this.port = process.env.PORT || 3000;
 		this.usuariosPath = "/api/usuarios";
 
 		//Middlewares
